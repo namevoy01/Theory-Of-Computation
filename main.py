72a2d73e6a2743709c79883b30c7555f5cdc01d3
muyumq <aphiwitsomphet@gmail.com>
2024-03-11 10:17:32 +0700

    a

--- main.py
+++ main.py
@@ -1,29 +1,41 @@
 from typing import Union
 
 from fastapi import FastAPI
-
+from fastapi.middleware.cors import CORSMiddleware
 from create_instance import create_instance
 
 controller = create_instance()
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['http://localhost:5173/', 'http://localhost:3000'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/select-event")
+@app.get("/select-event")
 def select_event(event_name : str):
     data = controller.select_event(event_name)
     return data
 
-@app.post("/select-show")
+@app.get("/select-show")
 def select_show(event_name : str, show_date:str, show_time:str):
     data = controller.select_show(event_name, show_date, show_time)
+    return data
+
+@app.get("/select-zone")
+def select_zone(account_id:str, event_name:str, show_date:str, show_time:str, zone_name:str):
+    data = controller.select_zone(account_id, event_name, show_date, show_time)
     return data
 
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     item = controller.search(item_id)
     return {"item_id": item}
+

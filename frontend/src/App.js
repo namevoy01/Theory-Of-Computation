fc9404c73e176886af04c619854167d08ad145d0
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-08-19 13:40:38 +0700

    Update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,23 +19,25 @@ function App() {
             <div class="flex flex-col space-y-6">
               <div>
                 <div class="flex items-center space-x-2">
-                  <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                  </svg>
+                <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
                   <span class="font-semibold text-lg text-gray-700">Home</span>
                 </div>
               </div>
               <div>
                 <div class="flex items-center space-x-2">
                   <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
                   </svg>
-                  <input type="text"className="font-semibold border border-violet-500 text-lg text-gray-700 bg-white rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                  <input type="text"className="font-semibold border border-violet-900 text-lg text-gray-700 bg-white rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
                       placeholder="Search"/>
                 </div>
               </div>
             </div>
             <div class="mt-8">
-              <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">Album</button>
-              <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">Artist</button>
+              <button class="bg-violet-900 text-white  rounded-full px-4 py-2 mr-2 focus:outline-none">Album</button>
+              <button class="bg-violet-900 text-white rounded-full px-4 py-2  focus:outline-none">Artist</button>
             </div>
 
             <div class="mt-6 space-y-4 overflow-y-auto max-h-80">
@@ -56,15 +58,15 @@ function App() {
               <div class="flex items-center space-x-4 rounded-full w-10 h-10">
                 <img src="https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png" alt="Artist" class="rounded-full" />
                 <div>
-                  <div class="font-bold text-gray-700">BOWKYLION</div>
+                  <div class="font-bold text-gray-700">Bowkylion</div>
                   <div class="text-sm text-gray-500">Artist</div>
                 </div>
               </div>
-              <div class="flex items-center space-x-4 rounded-full w-10 h-10">
-                <img src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png" alt="Artist" class="rounded-full" />
+              <div class="flex items-center inline-block space-x-4 rounded-full w-10 h-10">
+                <img src="https://i.pinimg.com/originals/1b/39/5e/1b395e47be82b4d18b21fcbc3d602d2b.jpg" alt="Artist" class="rounded-full" />
                 <div>
-                  <div class="font-bold text-gray-700">A-LIST</div>
-                  <div class="text-sm text-gray-500">Artist</div>
+                  <div class="font-bold text-gray-700">Downuea</div>
+                  <div class="text-sm text-gray-500">Artisxxxxt</div>
                 </div>
               </div>
               </div>
@@ -73,11 +75,11 @@ function App() {
           <div class="col-span-3 bg-white p-4 rounded-lg shadow-lg">
             <div class="flex items-center justify-between">
               <div>
-                <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">All</button>
-                <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">TOP 50</button>
+                <button class="bg-violet-900 text-white  rounded-full px-4 py-2 mr-2 focus:outline-none">All</button>
+                <button class="bg-violet-900 text-white rounded-full px-4 py-2  focus:outline-none">TOP 50</button>
               </div>
               <div class="bg-black-100 h-8 w-16 flex items-center justify-center">
-                <img src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png" alt="Artist" class="rounded-full" />
+                <img src="https://upload.wikimedia.org/wikipedia/commons/a/ae/Github-desktop-logo-symbol.svg" alt="Artist" class="rounded-full" />
               </div>
             </div>
 
@@ -90,8 +92,8 @@ function App() {
                   <div class="w-2/12 text-right">Time</div>
                 </div>
 
-                <div class="bg-violet space-y-4 shadow-lg">
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
+                <div class="bg-violet space-y-4">
+                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-lg">
                     <div class="w-1/12">1</div>
                     <div class="w-5/12 flex items-center space-x-2">
                       <img
@@ -108,7 +110,7 @@ function App() {
                     <div class="w-2/12 text-right">2:18</div>
                   </div>
 
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
+                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-lg">
                     <div class="w-1/12">2</div>
                     <div class=" w-5/12 flex items-center space-x-2">
                       <img
@@ -127,7 +129,7 @@ function App() {
                     <div class="w-2/12 text-right">3:48</div>
                   </div>
 
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
+                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-lg">
                     <div class="w-1/12">3</div>
                     <div class="w-5/12 flex items-center space-x-2">
                       <img
@@ -154,4 +156,4 @@ function App() {
 }
 
 export default App;
-// P
+//

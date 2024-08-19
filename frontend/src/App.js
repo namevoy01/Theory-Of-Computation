182d5062bac6c2807be8182220dd29e5104e7b6a
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-08-19 13:05:16 +0700

    F div

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState("");
+  const [data, setData] = useState('');
 
   useEffect(() => {
     // ใช้ URL ที่ระบุสำหรับ API
-    fetch("http://localhost:8000/api/data")
-      .then((response) => response.json())
-      .then((data) => setData(data.data))
-      .catch((error) => console.error("Error:", error));
+    fetch('http://localhost:8000/api/data')
+      .then(response => response.json())
+      .then(data => setData(data.data))
+      .catch(error => console.error('Error:', error));
   }, []);
 
   return (
@@ -19,120 +19,81 @@ function App() {
             <div class="flex flex-col space-y-6">
               <div>
                 <div class="flex items-center space-x-2">
-                  <svg
-                    class="w-6 h-6 text-gray-700"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  ></svg>
+                  <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  </svg>
                   <span class="font-semibold text-lg text-gray-700">Home</span>
                 </div>
               </div>
               <div>
                 <div class="flex items-center space-x-2">
-                  <svg
-                    class="w-6 h-6 text-gray-700"
-                    fill="none"
-                    stroke="currentColor"
-                    viewBox="0 0 24 24"
-                  ></svg>
-                  <input
-                    type="text"
-                    class="font-semibold border border-violet-500 text-lg text-gray-700 bg-white rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
-                    placeholder="Search"
-                  />
+                  <svg class="w-6 h-6 text-gray-700" fill="none" stroke="currentColor" viewBox="0 0 24 24">
+                  </svg>
+                  <input type="text"className="font-semibold border border-violet-500 text-lg text-gray-700 bg-white rounded py-2 px-4 focus:outline-none focus:ring-2 focus:ring-blue-500"
+                      placeholder="Search"/>
                 </div>
               </div>
             </div>
             <div class="mt-8">
-              <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">
-                Album
-              </button>
-              <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">
-                Artist
-              </button>
+              <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">Album</button>
+              <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">Artist</button>
             </div>
 
             <div class="mt-6 space-y-4 overflow-y-auto max-h-80">
-              <div class="flex items-center space-x-4">
-                <img
-                  src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTEj1SmQrJA78ga-YlqBOxRfT3NJTO2m1N3HWfO_NrtkqtdUGQv"
-                  alt="Artist"
-                  class="rounded-full w-10 h-10"
-                />
+              <div class="flex items-center space-x-4 rounded-full w-10 h-10">
+                <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTEj1SmQrJA78ga-YlqBOxRfT3NJTO2m1N3HWfO_NrtkqtdUGQv" alt="Artist" class="rounded-full" />
                 <div>
                   <div class="font-bold text-gray-700">2Ecstasy</div>
                   <div class="text-sm text-gray-500">Artist</div>
                 </div>
               </div>
-              <div class="flex items-center space-x-4">
-                <img
-                  src="https://www.thaiticketmajor.com/variety/img_content/imgeditor/S__77496392.jpg"
-                  alt="Artist"
-                  class="rounded-full w-10 h-10"
-                />
+              <div class="flex items-center space-x-4 rounded-full w-10 h-10">
+                <img src="https://www.thaiticketmajor.com/variety/img_content/imgeditor/S__77496392.jpg" alt="Artist" class="rounded-full" />
                 <div>
                   <div class="font-bold text-gray-700">Lipta</div>
                   <div class="text-sm text-gray-500">Artist</div>
                 </div>
               </div>
-              <div class="flex items-center space-x-4">
-                <img
-                  src="https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png"
-                  alt="Artist"
-                  class="rounded-full w-10 h-10"
-                />
+              <div class="flex items-center space-x-4 rounded-full w-10 h-10">
+                <img src="https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png" alt="Artist" class="rounded-full" />
                 <div>
                   <div class="font-bold text-gray-700">BOWKYLION</div>
                   <div class="text-sm text-gray-500">Artist</div>
                 </div>
               </div>
-              <div class="flex items-center space-x-4">
-                <img
-                  src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png"
-                  alt="Artist"
-                  class="rounded-full w-10 h-10"
-                />
+              <div class="flex items-center space-x-4 rounded-full w-10 h-10">
+                <img src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png" alt="Artist" class="rounded-full" />
                 <div>
                   <div class="font-bold text-gray-700">A-LIST</div>
                   <div class="text-sm text-gray-500">Artist</div>
                 </div>
               </div>
-            </div>
+              </div>
           </div>
 
           <div class="col-span-3 bg-white p-4 rounded-lg shadow-lg">
             <div class="flex items-center justify-between">
               <div>
-                <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">
-                  All
-                </button>
-                <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">
-                  TOP 50
-                </button>
+                <button class="bg-violet-800 text-white border border-black rounded-full px-4 py-2 mr-2 focus:outline-none">All</button>
+                <button class="bg-violet-800 text-white rounded-full px-4 py-2 border border-black focus:outline-none">TOP 50</button>
               </div>
               <div class="bg-black-100 h-8 w-16 flex items-center justify-center">
-                <img
-                  src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png"
-                  alt="Artist"
-                  class="rounded-full"
-                />
+                <img src="https://i.pinimg.com/originals/93/46/53/934653214719cf630e0f5cf9c746b364.png" alt="Artist" class="rounded-full" />
               </div>
             </div>
 
             <div class="mt-6">
               <div class="min-w-full bg-white">
-                <div class="w-full bg-white flex justify-between px-4 py-2">
-                  <div class="text-left">No</div>
-                  <div class="text-left">Name</div>
-                  <div class="text-left">Album</div>
-                  <div class="text-right">Time</div>
+                <div class="w-full bg-white flex items-center px-4 py-2">
+                  <div class="w-1/12 text-left">No</div>
+                  <div class="w-5/12 text-left">Name</div>
+                  <div class="w-4/12 text-left">Album</div>
+                  <div class="w-2/12 text-right">Time</div>
                 </div>
 
                 <div class="bg-violet space-y-4 shadow-lg">
                   <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
-                    <div>1</div>
-                    <div class="flex items-center space-x-2">
+                    <div class="w-1/12">1</div>
+                    <div class="w-5/12 flex items-center space-x-2">
                       <img
                         src="https://f.ptcdn.info/615/084/000/lxtkc0zaiBPvh1mi77z-o.jpg"
                         alt="Rockstar"
@@ -143,13 +104,13 @@ function App() {
                         <div class="text-sm text-gray-500">LISA</div>
                       </div>
                     </div>
-                    <div>Rockstar</div>
-                    <div class="text-right">2:18</div>
+                    <div class="w-4/12">Rockstar</div>
+                    <div class="w-2/12 text-right">2:18</div>
                   </div>
 
                   <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
-                    <div>2</div>
-                    <div class="flex items-center space-x-2">
+                    <div class="w-1/12">2</div>
+                    <div class=" w-5/12 flex items-center space-x-2">
                       <img
                         src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiP2OKii3peB3FGvp2liul841wFN8DbJuxYQ&s"
                         alt="THE VOYS"
@@ -162,13 +123,13 @@ function App() {
                         <div class="text-sm text-gray-500">INK WARUNTORN</div>
                       </div>
                     </div>
-                    <div>ลบไม่ได้ช่วยให้ลืม</div>
-                    <div class="text-right">3:48</div>
+                    <div class="w-4/12">ลบไม่ได้ช่วยให้ลืม</div>
+                    <div class="w-2/12 text-right">3:48</div>
                   </div>
 
                   <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2">
-                    <div>3</div>
-                    <div class="flex items-center space-x-2">
+                    <div class="w-1/12">3</div>
+                    <div class="w-5/12 flex items-center space-x-2">
                       <img
                         src="https://siamdara.com/cms/uploads/music/IMG-66627644050e00.75763144.jpg"
                         alt="DAY ONE"
@@ -179,8 +140,8 @@ function App() {
                         <div class="text-sm text-gray-500">PUNYARB</div>
                       </div>
                     </div>
-                    <div>DAY ONE</div>
-                    <div class="text-right">4:32</div>
+                    <div class="w-4/12">DAY ONE</div>
+                    <div class="w-2/12 text-right">4:32</div>
                   </div>
                 </div>
               </div>

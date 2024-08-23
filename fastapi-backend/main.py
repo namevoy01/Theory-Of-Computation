97acb296448be4db07a230c22dced4d81508ac7c
levywangx <147551403+levywangx@users.noreply.github.com>
2024-08-23 08:26:56 +0700

    พีชเองจ้า
    เขียน api กับเพิ่ม mockup

--- fastapi-backend/main.py
+++ fastapi-backend/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from mockup_data import allSong
+from typing import *
 
 app = FastAPI()
 
@@ -12,10 +14,55 @@ app.add_middleware(
     allow_headers=["*"],  # หรือระบุ headers ที่อนุญาต\
 )
 
-@app.get("/api/data")
-def get_data():
-    return {"data": "This is data from FastAPI"}
+@app.get("/get_all_song")
+def get_all_song():
+    return allSong
 
-@app.get("/")
-def home():
-    return {"data" : "Hello World!"}
+@app.get("/get_all_artist")
+def get_all_artist() -> List[Dict[str, Any]]:
+    artist_data = {}
+    
+    artist_id = 1
+    for song in allSong:
+        artist_name = song['artist']
+        if artist_name not in artist_data:
+            artist_data[artist_name] = {
+                'artistID': artist_id,
+                'artistName': artist_name,
+                'artistSong': []
+            }
+            artist_id += 1
+        
+        artist_data[artist_name]['artistSong'].append({
+            'id': song['id'],
+            'song': song['song'],
+            'img': song['img'],
+            'rank': song['rank']
+        })
+
+    return list(artist_data.values())
+
+@app.get("/get_artist/{artist_name}")
+def get_artist_song(artist_name: str) -> Dict[str, Any]:
+    artist_songs = {
+        'artistID': None,
+        'artistName': artist_name,
+        'artistSong': []
+    }
+
+    artist_id = 1
+    artist_found = False
+    for song in allSong:
+        if song['artist'] == artist_name:
+            if not artist_found:
+                artist_songs['artistID'] = artist_id
+                artist_found = True
+            artist_songs['artistSong'].append({
+                'id': song['id'],
+                'song': song['song'],
+                'img': song['img'],
+                'rank': song['rank']
+            })
+            artist_id += 1
+
+    return artist_songs
---
+++ fastapi-backend/mockup_data.py
@@ -0,0 +1,69 @@
+allSong = [
+    {
+        'id' : 1,
+        'artist' : 'Taylor Swift',
+        'song' : 'The Tortured Poets Department',
+        'img' : 'https://charts-static.billboard.com/img/2024/05/taylor-swift-9sy-thetorturedpoetsdepartment-g9f-180x180.jpg',
+        'rank' : '1'
+    },{
+        'id' : 2,
+        'artist' : 'Chappell Roan',
+        'song' : 'The Rise And Fall Of A Midwest Princess',
+        'img' : 'https://charts-static.billboard.com/img/2023/10/chappellroan-jg9-theriseandfallofamidwestprincess-6vr-180x180.jpg',
+        'rank' : '2'
+    },{
+        'id' : 3,
+        'artist' : 'Billie Eilish',
+        'song' : 'Hit Me Hard And Soft',
+        'img' : 'https://charts-static.billboard.com/img/2024/06/billie-eilish-lrt-hitmehardandsoft-c1a-180x180.jpg',
+        'rank' : '3'
+    },{
+        'id' : 4,
+        'artist' : 'Taylor Swift',
+        'song' : 'Folklore',
+        'img' : 'https://charts-static.billboard.com/img/2020/08/taylor-swift-9sy-folklore-hjz-180x180.jpg',
+        'rank' : '4'
+    }
+]
+
+artist = [
+    {
+        'artistID' : 1,
+        'artistName' : 'Taylor Swift',
+        'artistSong' : [
+            {
+                'id' : 1,
+                'song' : 'The Tortured Poets Department',
+                'img' : 'https://charts-static.billboard.com/img/2024/05/taylor-swift-9sy-thetorturedpoetsdepartment-g9f-180x180.jpg',
+                'rank' : '1' 
+            },{
+                'id' : 4,
+                'song' : 'Folklore',
+                'img' : 'https://charts-static.billboard.com/img/2020/08/taylor-swift-9sy-folklore-hjz-180x180.jpg',
+                'rank' : '4' 
+            }
+        ]
+    },{
+        'artistID' : 2,
+        'artistName' : 'Chappell Roan',
+        'artistSong' : [
+            {
+                'id' : 2,
+                'song' : 'The Rise And Fall Of A Midwest Princess',
+                'img' : 'https://charts-static.billboard.com/img/2023/10/chappellroan-jg9-theriseandfallofamidwestprincess-6vr-180x180.jpg',
+                'rank' : '2' 
+            }
+        ]
+    },{
+        'artistID' : 3,
+        'artistName' : 'Billie Eilish',
+        'artistSong' : [
+            {
+                'id' : 3,
+                'song' : 'Hit Me Hard And Soft',
+                'img' : 'https://charts-static.billboard.com/img/2024/06/billie-eilish-lrt-hitmehardandsoft-c1a-180x180.jpg',
+                'rank' : '3' 
+            }
+        ]
+    }
+]

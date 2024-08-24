e8a8eadd05a356f719f30512dfd5f124556de6ce
p <147586148+xzaren@users.noreply.github.com>
2024-08-24 21:34:55 +0700

    configAPI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,23 @@
 import React, { useEffect, useState } from "react";
 
 function App() {
-  const [data, setData] = useState("");
+  const [songs, setSongs] = useState([]);
+  const [artists, setArtists] = useState([]);
 
   useEffect(() => {
-    // ใช้ URL ที่ระบุสำหรับ API
-    fetch("http://localhost:8000/api/data")
-      .then((response) => response.json())
-      .then((data) => setData(data.data))
+    // เรียกทั้งสอง API พร้อมกัน
+    Promise.all([
+      fetch("http://localhost:8000/get_all_song").then((response) =>
+        response.json()
+      ),
+      fetch("http://localhost:8000/get_all_artist").then((response) =>
+        response.json()
+      ),
+    ])
+      .then(([songsData, artistsData]) => {
+        setSongs(songsData);
+        setArtists(artistsData);
+      })
       .catch((error) => console.error("Error:", error));
   }, []);
 
@@ -18,6 +28,7 @@ function App() {
           <div class="col-span-1 bg-white p-4 rounded-lg shadow-lg">
             <div class="flex flex-col space-y-6">
               <div>
+                
                 <div class="flex items-center space-x-2">
                   <svg
                     class="w-6 h-6 text-gray-700"
@@ -65,81 +76,35 @@ function App() {
               </button>
             </div>
 
-            <div class="mt-6 space-y-4 overflow-y-auto max-h-[80vh]">
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTEj1SmQrJA78ga-YlqBOxRfT3NJTO2m1N3HWfO_NrtkqtdUGQv"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20"
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">2Ecstasy</div>
-                  <div class="text-sm text-gray-500">Artist</div>
+            <div className="mt-6 space-y-4 overflow-y-auto max-h-[80vh]">
+              {artists.map((artist) => (
+                <div key={artist.artistID}>
+                  <div className="font-bold text-gray-700 text-lg mb-2">
+                    {artist.artistName}
+                  </div>
+                  {artist.artistSong.length > 0 && (
+                    <button
+                    key={artist.artistSong[0].id}
+                      className="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24"
+                    >
+                      <img
+                        src={artist.artistSong[0].img}
+                        alt={artist.artistSong[0].song}
+                        className="rounded-lg w-20 h-20"
+                      />
+                      <div>
+                        <div className="font-bold text-gray-700 text-lg">
+                        {artist.artistSong[0].song}
+                        </div>
+                        <div className="text-sm text-gray-500">
+                          {artist.artistName}
+                        </div>
+                      </div>
+                    </button>
+                  )}
                 </div>
-              </button>
-
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://www.thaiticketmajor.com/variety/img_content/imgeditor/S__77496392.jpg"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20"
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">Lipta</div>
-                  <div class="text-sm text-gray-500">Artist</div>
-                </div>
-              </button>
-
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://www.myband.co.th/uploads/20181230/94cab1a3d6aed152156b395b0fefd2fc.png"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20"
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">Bowkylion</div>
-                  <div class="text-sm text-gray-500">Artist</div>
-                </div>
-              </button>
-
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://i.pinimg.com/originals/1b/39/5e/1b395e47be82b4d18b21fcbc3d602d2b.jpg"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20"
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">Downuea</div>
-                  <div class="text-sm text-gray-500">Artist</div>
-                </div>
-              </button>
-
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://i.pinimg.com/originals/4f/fb/69/4ffb698cf4fbb010d692f6e8c14c3f2d.jpg"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20 "
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">XXXX</div>
-                  <div class="text-sm text-gray-500">Artist</div>
-                </div>
-              </button>
-
-              <button class="flex items-center space-x-4 rounded-lg p-4 border-solid border-2 border-white hover:border-gray-400 focus:outline-none w-full h-24">
-                <img
-                  src="https://i.pinimg.com/originals/5c/5d/a2/5c5da29b48e58e203b3511a1053f443e.jpg"
-                  alt="Artist"
-                  class="rounded-lg w-20 h-20"
-                />
-                <div>
-                  <div class="font-bold text-gray-700 text-lg">YYYY</div>
-                  <div class="text-sm text-gray-500">Artist</div>
-                </div>
-              </button>
-
+              ))}
             </div>
-
           </div>
 
 
@@ -192,91 +157,25 @@ function App() {
                 </div>
 
                 <div class="bg-violet space-y-4 overflow-y-auto max-h-[400px]">
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl ">
-                    <div class="w-1/12">1</div>
+                {songs.map((song, index) => (
+                  <div key={index} class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl ">
+                    <div class="w-1/12">{index + 1}</div>
                     <div class="w-5/12 flex items-center space-x-2">
                       <img
-                        src="https://f.ptcdn.info/615/084/000/lxtkc0zaiBPvh1mi77z-o.jpg"
-                        alt="Rockstar"
+                        src={song.img}
+                        alt={song.song}
                         class="rounded-lg w-20 h-20"
                       />
                       <div>
-                        <div class="font-bold text-gray-700">Rockstar</div>
-                        <div class="text-sm text-gray-500">LISA</div>
+                        <div class="font-bold text-gray-700">{song.song}</div>
+                        <div class="text-sm text-gray-500">{song.artist}</div>
                       </div>
                     </div>
-                    <div class="w-4/12">Rockstar</div>
-                    <div class="w-2/12 text-right">2:18</div>
+                    <div class="w-4/12">{song.song}</div>
+                    <div class="w-2/12 text-right">{song.rank}</div>
                   </div>
-
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl">
-                    <div class="w-1/12">2</div>
-                    <div class=" w-5/12 flex items-center space-x-2">
-                      <img
-                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiP2OKii3peB3FGvp2liul841wFN8DbJuxYQ&s"
-                        alt="THE VOYS"
-                        class="rounded-lg w-20 h-20"
-                      />
-                      <div>
-                        <div class="font-bold text-gray-700">ลบไม่ได้ช่วยให้ลืม</div>
-                        <div class="text-sm text-gray-500">INK WARUNTORN</div>
-                      </div>
-                    </div>
-                    <div class="w-4/12">ลบไม่ได้ช่วยให้ลืม</div>
-                    <div class="w-2/12 text-right">3:48</div>
-                  </div>
-
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl">
-                    <div class="w-1/12">3</div>
-                    <div class="w-5/12 flex items-center space-x-2">
-                      <img
-                        src="https://siamdara.com/cms/uploads/music/IMG-66627644050e00.75763144.jpg"
-                        alt="DAY ONE"
-                        class="rounded-lg w-20 h-20"
-                      />
-                      <div>
-                        <div class="font-bold text-gray-700">DAY ONE</div>
-                        <div class="text-sm text-gray-500">PUNYARB</div>
-                      </div>
-                    </div>
-                    <div class="w-4/12">DAY ONE</div>
-                    <div class="w-2/12 text-right">4:32</div>
-                  </div>
-
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl">
-                    <div class="w-1/12">4</div>
-                    <div class="w-5/12 flex items-center space-x-2">
-                      <img
-                        src="https://i.pinimg.com/originals/1b/39/5e/1b395e47be82b4d18b21fcbc3d602d2b.jpg"
-                        alt="New Song"
-                        class="rounded-lg w-20 h-20"
-                      />
-                      <div>
-                        <div class="font-bold text-gray-700">มักสาวคนมีเจ้าของ</div>
-                        <div class="text-sm text-gray-500">Downuea</div>
-                      </div>
-                    </div>
-                    <div class="w-4/12">X1</div>
-                    <div class="w-2/12 text-right">3:12</div>
-                  </div>
-                  <div class="border-b bg-violet-100 flex justify-between items-center px-4 py-2 shadow-xl rounded-xl">
-                    <div class="w-1/12">5</div>
-                    <div class="w-5/12 flex items-center space-x-2">
-                      <img
-                        src="https://i.pinimg.com/564x/e9/a3/47/e9a34783d9e719ce508e6bb573d6a12c.jpg"
-                        alt="New Song"
-                        class="rounded-lg w-20 h-20"
-                      />
-                      <div>
-                        <div class="font-bold text-gray-700">เหนืออยากมีเมียน้อย</div>
-                        <div class="text-sm text-gray-500">Downuea</div>
-                      </div>
-                    </div>
-                    <div class="w-4/12">Downueady</div>
-                    <div class="w-2/12 text-right">3:22</div>
-                  </div>
+                  ))}
                 </div>
-
               </div>
             </div>
           </div>

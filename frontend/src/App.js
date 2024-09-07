ce02b277dbceb69272bbae4514b44d0e34c9a8b2
Downueaa_ <162528777+Downuea2545@users.noreply.github.com>
2024-09-07 14:50:16 +0700

    search & click

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,8 +23,12 @@ function App() {
     setIsLoadingSongs(true);
 
     Promise.all([
-      fetch("https://theory-of-computation.onrender.com/get_all_song").then(response => response.json()),
-      fetch("https://theory-of-computation.onrender.com/get_all_artist").then(response => response.json()),
+      fetch("https://theory-of-computation.onrender.com/get_all_song").then(
+        (response) => response.json()
+      ),
+      fetch("https://theory-of-computation.onrender.com/get_all_artist").then(
+        (response) => response.json()
+      ),
     ])
       .then(([songsData, artistsData]) => {
         setSongs(Array.isArray(songsData) ? songsData : []);
@@ -32,10 +36,10 @@ function App() {
         setIsLoadingArtists(false);
         setIsLoadingSongs(false);
       })
-      .catch(error => console.error("Error:", error));
+      .catch((error) => console.error("Error:", error));
 
     const interval = setInterval(() => {
-      setCurrentImage(prev => (prev + 1) % imageUrls.length);
+      setCurrentImage((prev) => (prev + 1) % imageUrls.length);
     }, 1000);
 
     return () => clearInterval(interval);
@@ -46,10 +50,12 @@ function App() {
     setSearchKeyword(keyword);
 
     if (keyword.trim()) {
-      fetch(`https://theory-of-computation.onrender.com/search_songs?keyword=${keyword}`)
-        .then(response => response.json())
-        .then(data => setSearchResults(Array.isArray(data) ? data : []))
-        .catch(error => console.error("Error:", error));
+      fetch(
+        `https://theory-of-computation.onrender.com/search_songs?keyword=${keyword}`
+      )
+        .then((response) => response.json())
+        .then((data) => setSearchResults(Array.isArray(data) ? data : []))
+        .catch((error) => console.error("Error:", error));
     } else {
       setSearchResults([]);
     }
@@ -58,25 +64,38 @@ function App() {
   const handleArtistClick = (artistName) => {
     if (!searchKeyword.trim()) {
       const formattedArtistName = artistName.replace(/\//g, "-");
-      fetch(`https://theory-of-computation.onrender.com/get_artist/${encodeURIComponent(formattedArtistName)}`)
-        .then(response => response.json())
-        .then(data => {
-          const artistSongs = Array.isArray(data.artistSong) ? data.artistSong : [];
+      fetch(
+        `https://theory-of-computation.onrender.com/get_artist/${encodeURIComponent(
+          formattedArtistName
+        )}`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          const artistSongs = Array.isArray(data.artistSong)
+            ? data.artistSong
+            : [];
           setSelectedArtistSongs(
-            artistSongs.map(song => ({ ...song, artist: artistName }))
+            artistSongs.map((song) => ({ ...song, artist: artistName }))
           );
         })
-        .catch(error => console.error("Error:", error));
+        .catch((error) => console.error("Error:", error));
     }
   };
 
   const handleCsvDownload = () => {
-    window.location.href = "https://theory-of-computation.onrender.com/export_to_csv";
+    window.location.href =
+      "https://theory-of-computation.onrender.com/export_to_csv";
   };
 
   const toggleDropdown = () => {
-    setIsDropdownOpen(prev => !prev);
+    setIsDropdownOpen((prev) => !prev);
   };
+
+  const displayedSongs = searchKeyword.trim()
+    ? searchResults
+    : selectedArtistSongs.length
+    ? selectedArtistSongs
+    : songs;
 
   return (
     <div className="flex flex-col lg:flex-row h-screen overflow-hidden">
@@ -87,19 +106,40 @@ function App() {
           <div className="bg-gray-200 p-4 rounded-lg ml-2 -mt-1 h-40 overflow-auto">
             {/* Home Button */}
             <div className="flex items-center space-x-2 mb-6">
-              <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z" />
+              <svg
+                className="w-6 h-6 text-black"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M11.47 3.841a.75.75 0 0 1 1.06 0l8.69 8.69a.75.75 0 1 0 1.06-1.061l-8.689-8.69a2.25 2.25 0 0 0-3.182 0l-8.69 8.69a.75.75 0 1 0 1.061 1.06l8.69-8.689Z"
+                />
                 <path d="m12 5.432 8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 0 1-.75-.75v-4.5a.75.75 0 0 0-.75-.75h-3a.75.75 0 0 0-.75.75V21a.75.75 0 0 1-.75.75H5.625a1.875 1.875 0 0 1-1.875-1.875v-6.198a2.29 2.29 0 0 0 .091-.086L12 5.432Z"></path>
               </svg>
-              <button className="font-semibold text-lg text-black" onClick={() => window.location.reload()}>
+              <button
+                className="font-semibold text-lg text-black"
+                onClick={() => window.location.reload()}
+              >
                 Home
               </button>
             </div>
 
             {/* Search Input */}
             <div className="flex items-center space-x-2 mb-6">
-              <svg className="w-6 h-6 text-black" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+              <svg
+                className="w-6 h-6 text-black"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z"
+                />
               </svg>
               <input
                 type="text"
@@ -120,8 +160,19 @@ function App() {
               className="bg-gray-900 text-white rounded-lg p-2 focus:outline-none hover:bg-violet-300 hover:text-black"
               onClick={handleCsvDownload}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3" />
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                strokeWidth="1.5"
+                stroke="currentColor"
+                className="w-6 h-6"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M3 16.5v2.25A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75V16.5M16.5 12 12 16.5m0 0L7.5 12m4.5 4.5V3"
+                />
               </svg>
             </button>
           </div>
@@ -135,12 +186,14 @@ function App() {
             </button>
           </div>
           {/* Artist Cards with scrollable container */}
-          <div className={`flex-1 overflow-y-auto ${
-            isDropdownOpen ||
-            !window.matchMedia("(max-width: 1024px)").matches
-              ? "block"
-              : "hidden"
-          } lg:block`}>
+          <div
+            className={`flex-1 overflow-y-auto ${
+              isDropdownOpen ||
+              !window.matchMedia("(max-width: 1024px)").matches
+                ? "block"
+                : "hidden"
+            } lg:block`}
+          >
             {isLoadingArtists ? (
               <div className="text-center font-bold">Loading Artists...</div>
             ) : (
@@ -187,6 +240,7 @@ function App() {
         {/* Songs List */}
         <div className="min-w-full bg-gray-200 p-4 rounded-lg shadow-md -ml-6 -mt-5 -mr-2">
           <div className="w-full flex flex-col lg:flex-row px-4 py-2">
+            {/* Header */}
             <div className="w-full lg:w-1/12 text-left font-bold">#</div>
             <div className="w-full lg:w-5/12 text-left font-bold">Name</div>
             <div className="w-full lg:w-4/12 text-left font-bold">Artist</div>
@@ -196,7 +250,7 @@ function App() {
             <div className="text-center font-bold">Loading Songs...</div>
           ) : (
             <div className="space-y-4">
-              {songs.map((song, index) => (
+              {displayedSongs.map((song, index) => (
                 <div
                   key={index}
                   className="border-b bg-violet-100 flex items-center px-4 py-2 shadow-xl rounded-xl"

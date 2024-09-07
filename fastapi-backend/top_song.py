bf21ec7cf164b62793b11e88674dab6bc9b56da7
levywangx <147551403+levywangx@users.noreply.github.com>
2024-09-07 07:12:11 +0700

    deploy

--- fastapi-backend/top_song.py
+++ fastapi-backend/top_song.py
@@ -6,25 +6,28 @@ import csv
 
 ########## SCAP WEB ########## 
 
-r = requests.get('https://www.billboard.com/charts/billboard-200/')
-soup = BeautifulSoup(r.content, 'html.parser')
+def fetch_real_time_data():
+    r = requests.get('https://www.billboard.com/charts/billboard-200/')
+    soup = BeautifulSoup(r.content, 'html.parser')
+    
+    pattern = re.compile(r'^c.*trucate')
+    
+    h3_list = soup.find_all('h3', class_=pattern) 
+    span_list = soup.find_all('span', class_=pattern)
+    img_list = soup.find_all('img', class_=re.compile(r'^c-lazy-image')) 
 
-pattern = re.compile(r'^c.*trucate')
-
-h3_list = soup.find_all('h3', class_=pattern)
-
-span_list = soup.find_all('span', class_=pattern)
-
-img_list = soup.find_all('img', class_=re.compile(r'^c-lazy-image'))
-
-img_to_append = []
-for img in img_list:
-    chart_img = re.findall(r'.*charts(?!.*344x344).*', img['data-lazy-src'])
-    if chart_img:
-        img_to_append.extend(chart_img)
-img_to_append = img_to_append[1::]
+    img_to_append = []
+    for img in img_list:
+        chart_img = re.findall(r'.*charts(?!.*344x344).*', img['data-lazy-src'])
+        if chart_img:
+            img_to_append.extend(chart_img)
+    img_to_append = img_to_append[1::]
+    
+    return h3_list, span_list, img_to_append
 
 def artist_song_image():
+    h3_list, span_list, img_to_append = fetch_real_time_data()
+    
     x = []
     for i in range(len(h3_list)):
         x.append({'Artist' : span_list[i].string.strip(),
